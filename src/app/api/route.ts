008a2febe50452089fa9637926b7454228235600
Ezequiel M. Izaguirre <ezequielmizaguirre@gmail.com>
2024-06-22 11:24:20 -0300

    test v6

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,10 +5,19 @@ import { WebhookRequest, WebhookResponse } from "../private/types";
 
 export async function POST(request: Request) 
 {
-    try{
-    const data = await request.json() as WebhookRequest;
-    console.log("Data received: \n" + JSON.stringify(data));
-    }catch(e : any){};
+    try
+    {
+        const typed_data = await request.json() as WebhookRequest;
+        const raw_data = await request.json();
+
+        //const raw_body = await request.body
+        console.log('Raw json received: \n' + JSON.stringify(raw_data));
+        console.log('Typed json received: \n' + JSON.stringify(typed_data));
+    }
+    catch(e : any)
+    {
+        console.log('Parsing error');
+    };
 
     const response : WebhookResponse = 
     { 
@@ -23,28 +32,6 @@ export async function POST(request: Request)
         ]
     };
 
-    return Response.json(response);
-    
-}
-
-export async function GET(request: Request) 
-{
-    const data = await request.json() as WebhookRequest;
-    console.log("Data received: \n" + JSON.stringify(data));
-
-    const response : WebhookResponse = 
-    { 
-        fulfillmentMessages : 
-        [
-            {
-                text : 
-                {
-                    text : ['Este mensaje es una prueba']
-                }
-            }
-        ]
-    };
-    
     return Response.json(response);
     
 }
